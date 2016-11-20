8a42166070966f6d4c9e106e6883ad594c395d34
Nikolay <yalkaevnikolai@gmail.com>
2016-11-20 21:44:04 +0300

    Create Insert Hero.sql

---
+++ Insert Hero.sql
@@ -0,0 +1,149 @@
+INSERT into hero (name, class, role) 
+VALUES ('Pudge', 'Strength', 'Nuker');
+
+INSERT into hero (name, class, role) 
+VALUES ('Axe', 'Strength', 'Initiator');
+
+INSERT into hero (name, class, role) 
+VALUES ('Sven', 'Strength', 'Carry');
+
+INSERT into hero (name, class, role) 
+VALUES ('Vengeful Spirit', 'Agility', 'Support');
+
+INSERT into hero (name, class, role) 
+VALUES ('Earthshaker', 'Strength', 'Support');
+
+INSERT into hero (name, class, role) 
+VALUES ('Mirana', 'Agility', 'Support');
+
+INSERT into hero (name, class, role) 
+VALUES ('Juggernaut', 'Agility', 'Carry');
+
+INSERT into hero (name, class, role) 
+VALUES ('Lina', 'Intelligence', 'Nuker');
+
+INSERT into hero (name, class, role) 
+VALUES ('Storm Spirit', 'Intelligence', 'Initiator');
+
+INSERT into hero (name, class, role) 
+VALUES ('Windranger', 'Intelligence', 'Disabler');
+
+INSERT into hero (name, class, role) 
+VALUES ('Zeus', 'Intelligence', 'Nuker');
+
+INSERT into hero (name, class, role) 
+VALUES ('Shadow Shaman', 'Intelligence', 'Support');
+
+INSERT into hero (name, class, role) 
+VALUES ('Tinker', 'Intelligence', 'Nuker');
+
+INSERT into hero (name, class, role) 
+VALUES ('Jakiro', 'Intelligence', 'Support');
+
+INSERT into hero (name, class, role) 
+VALUES ('Tiny', 'Strength', 'Nuker');
+
+INSERT into hero (name, class, role) 
+VALUES ('Kunkka', 'Strength', 'Carry');
+
+INSERT into hero (name, class, role) 
+VALUES ('Omniknight', 'Strength', 'Support');
+
+INSERT into hero (name, class, role) 
+VALUES ('Anti-Mage', 'Agility', 'Carry');
+
+INSERT into hero (name, class, role) 
+VALUES ('Morphling', 'Agility', 'Escape');
+
+INSERT into hero (name, class, role) 
+VALUES ('Naga Siren', 'Agility', 'Initiator');
+
+INSERT into hero (name, class, role) 
+VALUES ('Ursa', 'Agility', 'Carry');
+
+INSERT into hero (name, class, role) 
+VALUES ('Bounty Hunter', 'Agility', 'Escape');
+
+INSERT into hero (name, class, role) 
+VALUES ('Gyrocopter', 'Agility', 'Carry');
+
+INSERT into hero (name, class, role) 
+VALUES ('Chen', 'Intelligence', 'Support');
+
+INSERT into hero (name, class, role) 
+VALUES ('Silencer', 'Intelligence', 'Support');
+
+INSERT into hero (name, class, role) 
+VALUES ('Disruptor', 'Intelligence', 'Initiator');
+
+INSERT into hero (name, class, role) 
+VALUES ('Huskar', 'Strength', 'Durable');
+
+INSERT into hero (name, class, role) 
+VALUES ('Alchemist', 'Strength', 'Carry');
+
+INSERT into hero (name, class, role) 
+VALUES ('Treant Protector', 'Strength', 'Initiator');
+
+INSERT into hero (name, class, role) 
+VALUES ('Brewmaster', 'Strength', 'Disabler');
+
+INSERT into hero (name, class, role) 
+VALUES ('Io', 'Strength', 'Support');
+
+INSERT into hero (name, class, role) 
+VALUES ('Timbersaw', 'Strength', 'Initiator');
+
+INSERT into hero (name, class, role) 
+VALUES ('Legion Commander', 'Strength', 'Carry');
+
+INSERT into hero (name, class, role) 
+VALUES ('Sniper', 'Agility', 'Carry');
+
+INSERT into hero (name, class, role) 
+VALUES ('Luna', 'Agility', 'Carry');
+
+INSERT into hero (name, class, role) 
+VALUES ('Shadow Fiend', 'Agility', 'Nuker');
+
+INSERT into hero (name, class, role) 
+VALUES ('Ogre Magi', 'Intelligence', 'Support');
+
+INSERT into hero (name, class, role) 
+VALUES ('Keeper of the Light', 'Intelligence', 'Support');
+
+INSERT into hero (name, class, role) 
+VALUES ('Skywrath Mage', 'Intelligence', 'Nuker');
+
+INSERT into hero (name, class, role) 
+VALUES ('Lich', 'Intelligence', 'Initiator');
+
+INSERT into hero (name, class, role) 
+VALUES ('Wraith King', 'Strength', 'Carry');
+
+INSERT into hero (name, class, role) 
+VALUES ('Spirit Breaker', 'Strength', 'Initiator');
+
+INSERT into hero (name, class, role) 
+VALUES ('Doom', 'Strength', 'Disabler');
+
+INSERT into hero (name, class, role) 
+VALUES ('Magnus', 'Strength', 'Initiator');
+
+INSERT into hero (name, class, role) 
+VALUES ('Riki', 'Agility', 'Escape');
+
+INSERT into hero (name, class, role) 
+VALUES ('Ember Spirit', 'Agility', 'Carry');
+
+INSERT into hero (name, class, role) 
+VALUES ('Venomancer', 'Agility', 'Support');
+
+INSERT into hero (name, class, role) 
+VALUES ('Witch Doctor', 'Intelligence', 'Support');
+
+INSERT into hero (name, class, role) 
+VALUES ('Bane', 'Intelligence', 'Support');
+
+INSERT into hero (name, class, role) 
+VALUES ('Oracle', 'Intelligence', 'Support');
